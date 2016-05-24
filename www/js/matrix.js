32bca0d2cfea165f1c1d3625a0fb660c0fab1caa
Polluxx <yuriy.kosakivsky@gmail.com>
2016-05-24 18:33:48 +0300

    add cordova matrix js

---
+++ www/js/matrix.js
@@ -0,0 +1,77 @@
+var matrix = [],
+    cols = 25,
+    rows = 20,i,j,
+    letters = [
+        'a', 'б', 'в', 'г', 'д',
+        'е', 'е', 'ж', 'з', 'и',
+        'й', 'к', 'л', 'м', 'н',
+        'о', 'п', 'р', 'с', 'т',
+        'у', 'ф', 'х', 'ц', 'ч',
+        'ш', 'щ', 'ї', 'ы', 'ь',
+        'е', 'ю', 'я'
+    ], lettersLen = letters.length;
+
+var shitHeap = [
+    'Неважно, кто мы такие, важно то, какой у нас план.',
+    'Я сам творю свою удачу!',
+    'Не проблемы должны толкать вас в спину, а вперед вести мечты.',
+    'Пессимист видит трудность в любой возможности; оптимист – видит возможность в любой трудности.',
+    'Мы — рабы своих привычек. Измени свои привычки, и изменится твоя жизнь…'
+], shitLength = shitHeap.length, initialShit = shitHeap[Math.floor(Math.random() * shitLength)],
+    board = new Board();
+
+console.log('board', board);
+
+function Board(cols, rows) {
+    this.colsNum = cols || 25;
+    this.rowsNum = rows || 20;
+    this.rows = [];
+    var index;
+
+    for(index=0;index<this.rowsNum;index++){
+        this.rows[index] = new Row(this.colsNum);
+    }
+}
+
+function Row(length) {
+    this.length = length || 25,
+    this.letters = [];
+    var index;
+    for(index=0;index<this.length;index++) {
+        this.letters[index] = new Letter(this, letters[Math.floor(Math.random() * lettersLen)].toUpperCase());
+    }
+}
+
+function Letter(parent, letter) {
+    this.name = letter || '';
+    this.row = parent;
+}
+
+Letter.prototype.set = function(letter) {
+    this.name = letter;
+}
+
+Letter.prototype.draw = function() {
+    // draw
+}
+
+
+function rebuild(cols, rows, letters, shit) {
+    var board = new Board();
+
+
+    for (i = 0; i < cols; i++) {
+        for (j = 0; j < rows; j++) {
+            if (!matrix[i]) matrix[i] = [];
+            matrix[i][j] = letters[Math.floor(Math.random() * lettersLen)].toUpperCase();
+        }
+    }
+
+
+    console.log(shit);
+    console.log('matrix', matrix);
+
+}
+
+//rebuild(cols, rows, letters, initialShit);
+
